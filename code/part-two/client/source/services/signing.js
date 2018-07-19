f88dd5e5a8f9ccc3b8960607590d4fdc700db9b5
ScottRudiger <scottrudiger@gmail.com>
2018-07-19 02:06:46 -0700

    feat(signing): complete signing service

--- code/part-two/client/source/services/signing.js
+++ code/part-two/client/source/services/signing.js
@@ -1,6 +1,14 @@
-import * as secp256k1 from 'secp256k1';
+import {
+  publicKeyCreate as createPublicKey,
+  sign as createSignature
+} from 'secp256k1';
 import { randomBytes, createHash } from 'crypto';
 
+// Returns a Buffer SHA-256 hash of a string or Buffer
+const sha256 = msg => createHash('sha256').update(msg).digest();
+
+// Converts a hex string to a Buffer
+const toBytes = hex => Buffer.from(hex, 'hex');
 
 /**
  * This module is essentially identical to part-one's signing module.
@@ -9,19 +17,14 @@ import { randomBytes, createHash } from 'crypto';
  * This function generates a random Secp256k1 private key, returning it as
  * a 64 character hex string.
  */
-export const createPrivateKey = () => {
-  // Enter your solution here
-
-};
+export const createPrivateKey = () => randomBytes(32).toString('hex');
 
 /**
  * Takes a hexadecimal private key and returns its public pair as a
  * 66 character hexadecimal string.
  */
-export const getPublicKey = privateKey => {
-  // Your code here
-
-};
+export const getPublicKey = privateKey => createPublicKey(toBytes(privateKey))
+  .toString('hex');
 
 /**
  * This convenience function did not exist in part-one's signing module, but
@@ -38,16 +41,15 @@ export const getPublicKey = privateKey => {
  *   //   publicKey: '0202694593ddc71061e622222ed400f5373cfa7ea607ce106cca...'
  *   // }
  */
-export const createKeys = () => {
-  // Your code here
-
-};
+export const createKeys = (privateKey = createPrivateKey()) => ({
+  privateKey,
+  publicKey: getPublicKey(privateKey),
+});
 
 /**
  * Takes a hex private key and a string message, returning a
  * hexadecimal signature.
  */
-export const sign = (privateKey, message) => {
-  // Your code here
-
-};
+export const sign = (privateKey, message) => createSignature(
+  sha256(message), toBytes(privateKey)
+).signature.toString('hex');

241c2f5ec0b86535e6bb7468d72cf637914b2e15
ScottRudiger <scottrudiger@gmail.com>
2018-07-20 00:33:17 -0700

    feat(encoding): implement encoding service

--- code/part-two/processor/services/encoding.js
+++ code/part-two/processor/services/encoding.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const stringify = require('json-stable-stringify');
+
 /**
  * A function that takes an object and returns it encoded as JSON Buffer.
  * Should work identically to the client version. Feel free to copy and paste
@@ -15,19 +17,13 @@
  *   deterministically! JSON is convenient, but you will need to sort
  *   your object's keys or random transactions may fail.
  */
-const encode = object => {
-  // Enter your solution here
-
-};
+const encode = object => Buffer.from(stringify(object));
 
 /**
  * A function that takes a JSON Buffer and decodes it into an object. Unlike
  * the client version, there is no need to handle base64 strings.
  */
-const decode = buffer => {
-  // Your code here
-
-};
+const decode = buffer => JSON.parse(buffer.toString());
 
 module.exports = {
   encode,
